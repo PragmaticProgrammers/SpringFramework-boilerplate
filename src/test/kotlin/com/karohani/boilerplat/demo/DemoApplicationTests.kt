3d2fb94f46e5a9d1897e3e95a1a4392c66992bc1
karohani <wilzilzor@gmail.com>
2022-07-01 19:26:44 +0900

    init-commit

---
+++ src/test/kotlin/com/karohani/boilerplat/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.karohani.boilerplat.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
